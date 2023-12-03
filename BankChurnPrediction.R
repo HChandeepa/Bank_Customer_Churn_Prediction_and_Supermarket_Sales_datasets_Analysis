20bde119e7e82f64699da3f77dfc34e27a3ad538
Heshan Chandeepa <118461284+HChandeepa@users.noreply.github.com>
2023-12-03 14:25:28 +0530

    K means Clustering

--- BankChurnPrediction.R
+++ BankChurnPrediction.R
@@ -66,16 +66,27 @@ split_ratio<-sample.split(dataset,SplitRatio = 0.75)
 training_dataset<-subset(dataset,split_ratio==TRUE)
 testing_dataset<-subset(dataset,split_ratio==FALSE)
 
-model<-naiveBayes(churn~.,data = training_dataset)
-predicted_results<-predict(model,newdata = testing_dataset)
+model1<-naiveBayes(churn~.,data = training_dataset)
+predicted_results<-predict(model1,newdata = testing_dataset)
 
-accuracy<-sum(predicted_results==testing_dataset$churn)/nrow(testing_dataset)
-print(round(accuracy,2))
+accuracy1<-sum(predicted_results==testing_dataset$churn)/nrow(testing_dataset)
+print(round(accuracy1,2))
 
 table(predicted_results)
 
 matrix<-table(testing_dataset$churn,predicted_results)
 confusionMatrix(matrix)
+
+#K-means clustering
+set.seed(150)
+cluster_results<-kmeans(dataset[,2:12],centers = 3,nstart = 25)
+cluster_results
+
+table(cluster_results$cluster,dataset$churn)
+
+library(cluster)
+clusplot(dataset,cluster_results$cluster,color = T,shade = T,labels = 1,lines = 0)
+table(cluster_results$cluster,dataset.class)
 
 
 library(ggplot2)
