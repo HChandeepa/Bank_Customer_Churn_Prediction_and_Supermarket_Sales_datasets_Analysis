c0a11f2134ec23ebfde8e5a706f48b14790bd64a
Heshan Chandeepa <118461284+HChandeepa@users.noreply.github.com>
2023-12-06 13:55:34 +0530

    Linear regression model

--- BankChurnPrediction.R
+++ BankChurnPrediction.R
@@ -77,16 +77,27 @@ table(predicted_results)
 matrix<-table(testing_dataset$churn,predicted_results)
 confusionMatrix(matrix)
 
-#K-means clustering
-set.seed(150)
-cluster_results<-kmeans(dataset[,2:12],centers = 3,nstart = 25)
-cluster_results
+#linear regression model
+# Assuming 'churn' is the target variable, you may need to split the dataset again as linear regression works differently than classification.
+set.seed(123)
+split_ratio <- sample.split(dataset, SplitRatio = 0.75)
+training_dataset <- subset(dataset, split_ratio == TRUE)
+testing_dataset <- subset(dataset, split_ratio == FALSE)
 
-table(cluster_results$cluster,dataset$churn)
+# Train the linear regression model
+linear_model <- lm(churn ~ ., data = training_dataset)
 
-library(cluster)
-clusplot(dataset,cluster_results$cluster,color = T,shade = T,labels = 1,lines = 0)
-table(cluster_results$cluster,dataset.class)
+# Predict using the model on the test dataset
+linear_predictions <- predict(linear_model, newdata = testing_dataset)
+
+# Calculate accuracy using confusion matrix
+linear_predictions <- ifelse(linear_predictions > 0.5, 1, 0)  # Assuming binary classification
+linear_accuracy <- sum(linear_predictions == testing_dataset$churn) / nrow(testing_dataset)
+print(round(linear_accuracy, 2))
+
+# Confusion matrix
+linear_matrix <- table(testing_dataset$churn, linear_predictions)
+confusionMatrix(linear_matrix)
 
 
 library(ggplot2)
